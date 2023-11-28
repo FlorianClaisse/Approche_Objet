093b2f0cc29af484bf8646fc7b8e604800bb8f0e
FlorianClaisse <floc.claisse@gmail.com>
2023-11-28 17:57:30 +0100

    Add mamager

--- Project/src/main/java/org/project/model/building/Building.java
+++ Project/src/main/java/org/project/model/building/Building.java
@@ -1,5 +1,6 @@
 package org.project.model.building;
 
+import org.jetbrains.annotations.NotNull;
 import org.project.model.resource.Buyable;
 import org.project.model.resource.Resource;
 
@@ -17,35 +18,39 @@ public class Building extends Buyable {
     private final Set<Resource> consomation;
     private final Set<Resource> production;
     private final int buildTime;
+    private int remainingTime;
 
-    protected Building(Type type,
-                       int nbHabitants,
-                       int minWorkers,
-                       int price,
-                       Set<Resource> buildRequirements,
-                       Set<Resource> consomation,
-                       Set<Resource> production,
-                       int buildTime)
-    {
+    protected Building(@NotNull Type type, int nbHabitants, int minWorkers, int price, Set<Resource> buildRequirements, Set<Resource> consomation, Set<Resource> production, int buildTime) {
         super(price);
-        if (type == null) throw new NullPointerException("You are trying to create a building with a null type.");
         this.type = type;
         this.level = 1;
         this.nbHabitants = nbHabitants;
         this.minWorkers = minWorkers;
+        this.maxWorkers = minWorkers;
         this.currentWorkers = minWorkers;
         this.buildRequirements = buildRequirements;
         this.consomation = consomation;
         this.production = production;
         this.buildTime = buildTime;
+        this.remainingTime = buildTime;
+    }
+
+    public int getNbHabitants() { return this.nbHabitants; }
+    public int getMinWorkers() { return this.minWorkers; }
+    public int getCurrentWorkers() { return this.currentWorkers; }
+    public List<Resource> getBuildRequirements() { return this.buildRequirements.stream().toList(); }
+    public List<Resource> getConsomation() { return this.consomation.stream().toList(); }
+    public List<Resource> getProduction() { return this.production.stream().toList(); }
+
+    public void removeOnePeriod() { this.remainingTime--; }
+    public boolean isBuilt() { return this.remainingTime == 0; }
+
+    public boolean canAddWorkers(int value) {
+        return (this.currentWorkers + value) <= this.maxWorkers;
     }
 
     public boolean canRemoveWorkers(int value) {
         return (this.currentWorkers - value) >= this.minWorkers;
-    }
-
-    public boolean canAddWorkers(int value) {
-        return (this.currentWorkers + value) <= this.maxWorkers;
     }
 
     public void addWorkers(int value) {
@@ -56,10 +61,6 @@ public class Building extends Buyable {
     public void removeWorkers(int value) {
         if (!canRemoveWorkers(value)) throw new IllegalStateException("Can't remove workers please use canRemoveWorkers()");
         this.currentWorkers -= value;
-    }
-
-    public List<Resource> getBuildRequirements() {
-        return this.buildRequirements.stream().toList();
     }
 
     public enum Type {
--- Project/src/main/java/org/project/model/gameengine/Manager.java
+++ Project/src/main/java/org/project/model/gameengine/Manager.java
@@ -30,38 +30,38 @@ public final class Manager {
         this.delegate = delegate;
     }
 
-    // TODO: Fix
-    public boolean addBuilding(@NotNull Building.Type buildingType) {
-        Building building;
-        building = switch (buildingType) {
-            case FARM -> BuildingFactory.makeFarm();
-            case HOUSE -> BuildingFactory.makeHouse();
-            case QUARRY -> BuildingFactory.makeQuarry();
-            case STEEL_MILL -> BuildingFactory.makeSteelMill();
-            case LUMBER_MILL -> BuildingFactory.makeLumberMill();
-            case CEMENT_PLANT -> BuildingFactory.makeCementPlant();
-            case TOOL_FACTORY -> BuildingFactory.makeToolFactory();
-            case APARTMENT_BUILDING -> BuildingFactory.makeApartment();
-            case WOODEN_CABIN -> BuildingFactory.makeWoodenCabin();
-        };
+    public void update() {
+        productResources();
+        consumeResources();
 
-        if (!this.delegate.canConstruct(building.getBuildRequirements())) {
-            return false;
+        List<Integer> toBeRemoved = new ArrayList<>();
+        for (Map.Entry<Integer, Building> entry: this.underConstruction.entrySet()) {
+            Building building = entry.getValue();
+            building.removeOnePeriod();
+            if (building.isBuilt()) {
+                this.habitants.addQuantity(building.getNbHabitants());
+                this.workers.addQuantity(building.getCurrentWorkers());
+                toBeRemoved.add(entry.getKey());
+                this.constructedBuildings.put(BUILDING_COUNTER, building);
+                BUILDING_COUNTER++;
+            }
         }
+        toBeRemoved.forEach(this.underConstruction::remove);
+    }
+
+    // TODO: Fix le changer de tableau une fois construit
+    public boolean addBuilding(@NotNull Building.Type type) {
+        Building building = getBuilding(type);
+        // Si on ne possede pas assez d'habitant libre pour travailler.
+        if ((this.freeInhabitant() + building.getNbHabitants()) - building.getMinWorkers() < 0) return false;
+        // Le delegate doit verifier qu'il possede assez de resource
+        if (!this.delegate.canConstruct(building.getBuildRequirements())) return false;
 
         this.underConstruction.put(UNDER_COUNTER, building);
         UNDER_COUNTER++;
 
         this.delegate.removeFromStock(building.getBuildRequirements());
         return true;
-    }
-
-    public void consumeResources() {
-        Set<Resource> toBeConsumed = new HashSet<>();
-
-        for (Building building: this.constructedBuildings.values()) {
-
-        }
     }
 
     public boolean removeExistingBuilding(int key) {
@@ -73,18 +73,26 @@ public final class Manager {
     }
 
     public boolean addWorkers(int key, int value) {
+        if (freeInhabitant() - value < 0) return false;
         if (this.constructedBuildings.containsKey(key)) {
             Building building = this.constructedBuildings.get(key);
             if (building.canAddWorkers(value)) {
+                this.habitants.removeQuantity(value);
+                this.workers.addQuantity(value);
                 building.addWorkers(value);
+                return true;
             }
+            return false;
         }
+        return false;
     }
 
     public boolean removeWorkers(int key, int value) {
         if (this.constructedBuildings.containsKey(key)) {
             Building building = this.constructedBuildings.get(key);
             if (building.canRemoveWorkers(value)) {
+                this.habitants.addQuantity(value);
+                this.workers.removeQuantity(value);
                 building.removeWorkers(value);
                 return true;
             }
@@ -93,12 +101,12 @@ public final class Manager {
         return false;
     }
 
-    // TODO: Fix
     private boolean removeBuilding(boolean under, int key) {
         Map<Integer, Building> buildings = under ? this.underConstruction : this.constructedBuildings;
         if (buildings.containsKey(key)) {
             Building building = buildings.remove(key);
-
+            this.habitants.removeQuantity(building.getNbHabitants());
+            this.workers.removeQuantity(building.getCurrentWorkers());
             if (under) {
                 List<Resource> resources = new ArrayList<>();
                 building.getBuildRequirements().forEach(it -> {
@@ -109,5 +117,35 @@ public final class Manager {
             return true;
         }
         return false;
+    }
+
+    private int freeInhabitant() { return this.habitants.getQuantity() - this.workers.getQuantity(); }
+
+    private Building getBuilding(Building.Type type) {
+        return switch (type) {
+            case FARM -> BuildingFactory.makeFarm();
+            case HOUSE -> BuildingFactory.makeHouse();
+            case QUARRY -> BuildingFactory.makeQuarry();
+            case STEEL_MILL -> BuildingFactory.makeSteelMill();
+            case LUMBER_MILL -> BuildingFactory.makeLumberMill();
+            case CEMENT_PLANT -> BuildingFactory.makeCementPlant();
+            case TOOL_FACTORY -> BuildingFactory.makeToolFactory();
+            case APARTMENT_BUILDING -> BuildingFactory.makeApartment();
+            case WOODEN_CABIN -> BuildingFactory.makeWoodenCabin();
+        };
+    }
+
+    private void consumeResources() {
+        List<Resource> toBeConsumed = new ArrayList<>();
+        this.constructedBuildings.values().forEach(it -> toBeConsumed.addAll(it.getConsomation()));
+        // Consommation de resource par les habitants
+        toBeConsumed.add(new Material(FOOD, this.habitants.getQuantity()));
+        this.delegate.removeFromStock(toBeConsumed);
+    }
+
+    private void productResources() {
+        List<Resource> toBeProducted = new ArrayList<>();
+        this.constructedBuildings.values().forEach(it -> toBeProducted.addAll(it.getProduction()));
+        this.delegate.removeFromStock(toBeProducted);
     }
 }
