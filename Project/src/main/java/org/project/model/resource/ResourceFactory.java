596d5feb02a6df1802248824da3c0ce8c4c3b624
Florian Claisse <floc.claisse@gmail.com>
2023-12-05 08:27:58 +0100

    add material in factory

--- Project/src/main/java/org/project/model/resource/ResourceFactory.java
+++ Project/src/main/java/org/project/model/resource/ResourceFactory.java
@@ -17,4 +17,18 @@ public final class ResourceFactory {
     public static Material cement() { return new Material(CEMENT); }
     public static Material lumber() { return new Material(LUMBER); }
     public static Material tools() { return new Material(TOOLS); }
+
+    public static Material material(Material.Type type) {
+        return switch (type) {
+            case STONE -> stone();
+            case WOOD -> wood();
+            case COAL -> coal();
+            case FOOD -> food();
+            case IRON -> iron();
+            case STEEL -> steel();
+            case TOOLS -> tools();
+            case CEMENT -> cement();
+            case LUMBER -> lumber();
+        };
+    }
 }
